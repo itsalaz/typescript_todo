510c84ed9a7a13fbb1007cd10564bf7db8c7fbca
Lizzie <itsalaz@icloud.com>
2025-09-30 12:27:48 -0400

    delete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,11 @@ const App: FC = () => {
 
   }
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(todoList.filter((task) => {
+      return task.taskName != taskNameToDelete
+    }))
+  }
 
 
   return <div className='App'>
@@ -49,7 +54,7 @@ const App: FC = () => {
       <button onClick={addTask}>Add Task</button>
       <div className='todoList'>
         {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task}/>
+          return <TodoTask key={key} task={task} completeTask={completeTask}/>
         })}
 
       </div>
--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { ITask } from '../Interfaces'
+import React from "react";
+import { ITask } from "../Interfaces";
 
 interface Props {
-    task: ITask
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
 }
 
-const TodoTask = ({ task}: Props) => {
-    return (
-        <div className="task">
-            <span>{task.taskName} </span>
-            <span>{task.deadline}</span>  
-        <button>X</button>
-        </div>
-    )
-}
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
 
 export default TodoTask;
